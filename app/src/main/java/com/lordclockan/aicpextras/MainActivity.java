f2e1201db0ed38540ce01392642dec586c8bee6e
LorDClockaN <davor@losinj.com>
2015-11-13 23:11:13 +0100

    Reformat and reorganize code

--- app/src/main/java/com/lordclockan/aicpextras/MainActivity.java
+++ app/src/main/java/com/lordclockan/aicpextras/MainActivity.java
@@ -1,10 +1,7 @@
 package com.lordclockan.aicpextras;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -15,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity
@@ -107,7 +103,7 @@ public class MainActivity extends AppCompatActivity
 
         Class fragmentClass;
 
-        switch(id) {
+        switch (id) {
             case R.id.nav_display_animations:
                 fragmentClass = DisplayAnimationsActivity.class;
                 break;
